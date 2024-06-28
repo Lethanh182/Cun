03cd3c5033f47d5fb6410b632ece1c250eb2a5ac
Lethanh182 <thanhtl181224@gmail.com>
2024-06-28 19:22:34 +0700

    Add files via upload

---
+++ Q3.3.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int is_prime(int num) {
+    if (num <= 1) return 0;
+    for (int i = 2; i * i <= num; i++) {
+        if (num % i == 0) return 0;
+    }
+    return 1;
+}
+
+int main() {
+    int n;
+    printf("Nhap so luong phan tu: ");
+    scanf("%d", &n);
+    int arr[n];
+    printf("Nhap vao % so nguyen : ", n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+    int found = 0;
+    printf("So nguyen to: ");
+    for (int i = 0; i < n; i++) {
+        if (is_prime(arr[i])) {
+            printf("%d ", arr[i]);
+            found = 1;
+        }
+    }
+    if (!found) {
+        printf("Khong co so nguyen to");
+    }
+    printf("\n");
+
+    return 0;
+}
